78e76c461b9d4ce5db7893266dd2a5a43f6e9fb1
editspace <editspace@protonmail.com>
2024-06-27 16:20:10 -0700

    updated page

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Page() {
   return (
     <div className="container mx-auto p-4">
     <h1 className="header font-bold mb-4 text-center">The Industrialists</h1>
+    <p className="text-center">editspace is our founder and our co-founder is Bob_Plaz_Gamez</p>
     <div className="w-full md:w-1/2 lg:w-1/3 mx-auto h-96"> {/* Adjust height as needed */}
       <EvervaultCard text="editspace" className="my-custom-class h-full" />
     </div>
