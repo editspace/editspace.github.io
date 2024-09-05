502be53ed88215a6222a44ee1e6df821871f1dd5
editspace <editspace@protonmail.com>
2024-09-04 17:30:25 -0700

    added discord server link

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,8 @@ export default function Page() {
       <p className="text-center">
         editspace is our founder and our co-founder is Bob_Plaz_Gamez
       </p>
+      
+      <a href=" https://discord.gg/mEpm8FqPTk" className="text-sky-400 hover:text-sky-600">Discord Server</a> 
       <div className="flex flex-wrap justify-center space-x-4">
         <div className="w-full md:w-1/2 lg:w-1/3  h-96">
           <EvervaultCard text="editspace" className="class1 h-full" />
